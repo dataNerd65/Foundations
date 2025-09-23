b10c8ad9bcd21d3e5ec1d7b47c656002ba80a126
dataNerd65 <pkiarie811@gmail.com>
2025-09-23 21:00:26 +0300

    Laying up the plan for the bank

--- Practical_Java/Banker.java
+++ Practical_Java/Banker.java
@@ -1,6 +1,28 @@
 import java.util.Scanner;
+
 public class Banker{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
+        System.out.println();
+        System.out.println("**** Welcome To ABSA ATM ****\n");
+        String customerName;
+
+        System.out.print("Enter your name:  ");
+        customerName = sc.nextLine();
+        System.out.println();
+
+        System.out.printf("Hello there %s!%n", customerName);
+
+        System.out.println("What would you like to do today?\n");
+
+        System.out.println("====== MENU =======\n"
+                          +"1. Check Balance \n"
+                         + "2. Deposit Money \n"
+                         + "3. Withdraw Money \n"
+                         + "4. Exit\n"
+                         + "===================");
+
+        sc.close();
     }
 }
